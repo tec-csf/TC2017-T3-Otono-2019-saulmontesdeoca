abf7ecd2f4a9413c38e9704c8ae935a2e19a1dc4
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-10-03 17:35:10 -0500

    Algoritmo resuelto

--- CamioneroConPrisa.cpp
+++ CamioneroConPrisa.cpp
@@ -8,3 +8,31 @@ desea detenerse a abastecer gasolina el menor número de veces posible.
 Diseñe un algoritmo eficiente que determine en qué gasolineras
 tiene que parar el camionero.
 */
+
+#include <iostream>
+
+
+using namespace std;
+
+void calcularParadas(int n, int *mapa, int gasolineras){
+    int kmRecorridos = 0;
+    cout<<"Se tiene que parar en las gases: ";
+    for (int i = 0; i < gasolineras-1; i++){
+        if (n-kmRecorridos <= (mapa[i+1]+mapa[i]))
+        {
+            cout<<" "<<i+1;//Imprimiendo donde tiene que cargar gas
+            kmRecorridos = 0;
+        } else{
+           /*Puede continuar*/
+           kmRecorridos+=mapa[i];
+        }
+    }
+}
+
+int main(){
+    int n = 10;
+    int gasolineras = 6;
+    int mapa[6] = {2,10,3,5,1,6};
+    calcularParadas(n,mapa,gasolineras);
+    return 0;
+}
